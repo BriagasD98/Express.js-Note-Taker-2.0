57a26b193929ac2be2d8666f0981015465256f00
BriagasD98 <briagasdavid@yahoo.com>
2021-07-26 00:10:47 -0500

    html routes added and tested

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 // Dependencies
 const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
 const fs = require('fs');
 const path = require('path');
 const db = require('./db/db.json');
 
-const app = express();
-const PORT = process.env.PORT || 3001;
-
+// Express
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// htmlRoutes
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+});
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'))
+});
 
 // Server listens and initializes
 app.listen(PORT, () => {
